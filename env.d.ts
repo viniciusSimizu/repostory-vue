86643088c1c92f1fc208ac017605d26f8fe155d5
“viniciussimizu” <vinicius.simizu@ekaizen.digital>
2022-11-09 15:29:24 -0300

    fix(public api): variable api url

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_BASE_URL: string
     readonly VITE_BASEURL_API: string
     readonly VITE_CLIENT_ID: string
     readonly VITE_CLIENT_SECRET: string
--- src/axios/api-public.axios.ts
+++ src/axios/api-public.axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
+import * as process from 'process'
 
 export const apiPublicAxios = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: process.env.VITE_BASEURL_API,
 })
